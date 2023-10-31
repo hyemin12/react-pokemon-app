97e90a9ce058722ae2aab34b750314e94764868f
hyemin12 <khnm31@naver.com>
2023-10-31 16:58:36 +0900

    feat: useDebounce hook 사용하는 검색 기능 추가

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import PokeCard from "../../components/PokeCard";
+import useDebounce from "../../hooks/useDebounce";
 
 const MainPage = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   const [offset, setOffset] = useState(0);
   const limit = 20;
 
@@ -24,10 +28,54 @@ const MainPage = () => {
     fetchPokeData(true);
   }, []);
 
+  const searchInputHandler = async (searchTerm) => {
+    if (searchTerm.length > 0) {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon/" + searchTerm
+        );
+        const pokemonData = {
+          url: "https://pokeapi.co/api/v2/pokemon/" + response.data.id,
+          name: searchTerm,
+        };
+        setPokemons([pokemonData]);
+      } catch (error) {
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      // 검색했다가 지우면 처음 나와야하는 포켓몬들 보여주기
+      fetchPokeData(true);
+    }
+  };
+
+  useEffect(() => {
+    searchInputHandler(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
   return (
     <article className="pt-6">
       <header className="flex flex-col gap-2 w-full px-4 z-50">
-        Input form
+        <div className="relative z-50">
+          <form
+            className="relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto"
+            onSubmit={searchInputHandler}
+          >
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
+            />
+
+            <button
+              type="submit"
+              className="text-xs bg-slate-900  text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700"
+            >
+              검색
+            </button>
+          </form>
+        </div>
       </header>
       <section className="pt-6 flex flex-col justify-content items-center overflow-auto z-0">
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
