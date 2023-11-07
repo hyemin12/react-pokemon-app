566c17b4fa877f4f2b0e8534606cc3c64111c326
hyemin12 <khnm31@naver.com>
2023-11-07 11:18:43 +0900

    refactor: useOnClickOutSide훅 타입 지정

--- src/hooks/useOnClickOutSide.ts
+++ src/hooks/useOnClickOutSide.ts
@@ -1,18 +1,15 @@
-// 모달창 밖을 클릭하면 함수를 호출하는 리스너 등록하기
-
-import { ReactNode, useEffect } from "react";
+import { useEffect } from "react";
 
 const useOnClickOutSide = (
   ref: React.RefObject<HTMLDivElement>,
   callback: () => void
 ) => {
-  console.log(callback);
   useEffect(() => {
-    const listener = (e: any) => {
+    const listener = (e: MouseEvent) => {
       // 1. 어디 클릭하는지 구분
       // 모달 창 클릭하면 return
       // 모달 창 밖 클릭하면 callback 함수 실행
-      if (!ref.current || ref.current.contains(e.target)) {
+      if (!ref.current || ref.current.contains(e.target as Node)) {
         return;
       }
       callback();
